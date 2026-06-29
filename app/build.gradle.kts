b0132f7a38dc3380f418103e5a76c65d556ae7f5
sozo786 <zohaibburat1973@gmail.com>
2026-06-29 21:56:59 +0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,6 @@ android {
       isCrunchPngs = false
       isMinifyEnabled = false
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-      signingConfig = signingConfigs.getByName("release")
     }
     
   }
